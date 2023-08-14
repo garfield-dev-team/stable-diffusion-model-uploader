ffb28466112d1f1e585741fad972cf219f18e682
dongjiacheng <dongjiacheng@corp.netease.com>
2023-08-14 09:29:41 +0800

    feat: 逻辑优化

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -3,29 +3,72 @@ package client
 import (
 	"bytes"
 	"context"
+	"crypto/md5"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
 	"time"
 )
 
+func TestGetFileSize(t *testing.T) {
+	url := "http://localhost:8066/vendors-node_modules_react-dom_client_js-node_modules_webpack-dev-server_client_index_js_prot-8907fc.js"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("===len", len(data))
+	log.Printf("===sum: %x\n", md5.Sum(data))
+}
+
+func TestDownloadChunk(t *testing.T) {
+	url := "http://localhost:8066/vendors-node_modules_react-dom_client_js-node_modules_webpack-dev-server_client_index_js_prot-8907fc.js"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	buf := make([]byte, 1024)
+	res := make([]byte, 0)
+	for {
+		n, err := resp.Body.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		res = append(res, buf[:n]...)
+	}
+	log.Printf("===sum: %x\n", md5.Sum(res))
+}
+
 func TestDownloadRange(t *testing.T) {
 	_, cancel := context.WithTimeout(context.Background(), 1*time.Hour)
 	defer cancel()
-	url := "https://civitai.com/api/download/models/111612"
-	var aliClient = &AliClient{chunkSize: 10 * 1024 * 1024}
-	err := aliClient.getFileMeta(url)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	//url := "https://civitai.com/api/download/models/111612"
+	url := "http://localhost:8066/vendors-node_modules_react-dom_client_js-node_modules_webpack-dev-server_client_index_js_prot-8907fc.js"
+	//var aliClient = &AliClient{chunkSize: 10 * 1024 * 1024}
+	var aliClient = &AliClient{chunkSize: 1024, fileSize: 1188339}
+	checksum := "0f3844283d8fdbb7a21b337b91457c1e"
+	//err := aliClient.getFileMeta(url)
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
 
 	totalIter := aliClient.fileSize / aliClient.chunkSize
 	log.Println("===fileSize", aliClient.fileSize)
 	log.Println("===chunkSize", aliClient.chunkSize)
 	log.Println("===totalIter", totalIter)
+	var buf bytes.Buffer
 	cnt := 0
 	iter := 0
 	for i := 0; i <= totalIter; i++ {
@@ -46,15 +89,19 @@ func TestDownloadRange(t *testing.T) {
 		}
 		iter += 1
 		cnt += len(data)
+		n, _ := buf.Write(data)
 
 		log.Println("===current", iter)
 		log.Println("===byte read", len(data))
+		log.Println("===byte write", n)
 		log.Println("===byte total", cnt)
 	}
 
 	log.Println("[success] result:")
 	log.Printf("totalIter: %d, iter: %d\n", totalIter, iter)
 	log.Printf("fileSize: %d, bytes send: %d\n", aliClient.fileSize, cnt)
+	log.Printf("===sum %x\n", md5.Sum(buf.Bytes()))
+	log.Println("===expect", checksum)
 }
 
 func TestAliClient_UploadChunk(t *testing.T) {
