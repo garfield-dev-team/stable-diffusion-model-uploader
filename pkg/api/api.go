4d999f666bfcde862f70c69fa3192a8c85db4de9
dongjiacheng <dongjiacheng@corp.netease.com>
2023-08-14 14:12:58 +0800

    feat: 逻辑优化

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -17,7 +17,7 @@ func FetchModelList() []*model.IModelDetailDTO {
 	if err != nil {
 		panic(fmt.Errorf("fail to create req: %w", err))
 	}
-	req.Header.Set("Cookie", "HAS_VISIT_SD_WEBUI=true; EDUWEBDEVICE=61001fcbcb9f41e6a0317483f66dae9b; EDU-YKT-MODULE_GLOBAL_PRIVACY_DIALOG=true; OUTFOX_SEARCH_USER_ID_NCOO=307420402.20727146; hb_MA-BFF5-63705950A31C_source=sd.study.163.com; NTESSTUDYSI=3cc26eec984a4f9bb214496034bcb7ea; utm=eyJjIjoiIiwiY3QiOiIiLCJpIjoiIiwibSI6IiIsInMiOiIiLCJ0IjoiIn0=|aHR0cHM6Ly9zdHVkeS4xNjMuY29tLw==; STUDY_SESS=\"pzx2LuQgFzdPhSyxfC22K/ZfSVwfSQoaWPxZH3AwpPEYTBRB0PaKsPVbd/mH2Sh9lTS2z38pY/CwHu3jaiHvZ3Ns7IVgVhVb3kWw90+U7+g2GezebVhZJ1iKgKbmBbur8eidZi8tyxWGICpjqp2QMUDTl4E8BVVA8n6AW7Yszk8Lhur2Nm2wEb9HcEikV+3FTI8+lZKyHhiycNQo+g+/oA==\"; STUDY_INFO=\"oP4xHuH_Yhivb2un05qwMVbar_Qk|6|1031410010|1691976511976\"")
+	req.Header.Set("Cookie", "HAS_VISIT_SD_WEBUI=true; EDUWEBDEVICE=61001fcbcb9f41e6a0317483f66dae9b; EDU-YKT-MODULE_GLOBAL_PRIVACY_DIALOG=true; OUTFOX_SEARCH_USER_ID_NCOO=307420402.20727146; hb_MA-BFF5-63705950A31C_source=sd.study.163.com; utm=\"eyJjIjoiIiwiY3QiOiIiLCJpIjoiIiwibSI6IiIsInMiOiIiLCJ0IjoiIn0=|aHR0cHM6Ly9zZC5zdHVkeS4xNjMuY29tLw==\"; STUDY_SESS=\"pzx2LuQgFzdPhSyxfC22K/ZfSVwfSQoaWPxZH3AwpPEYTBRB0PaKsPVbd/mH2Sh9lTS2z38pY/CwHu3jaiHvZwdKxsd+A4sQ8z6Zn74+ld3RmDNpMv76/n/T0nEjkAeXBjuH//koZcgBpm99y5p6REWrdTxX1zv/jpZ14OalGUILhur2Nm2wEb9HcEikV+3FTI8+lZKyHhiycNQo+g+/oA==\"; STUDY_INFO=\"oP4xHuH_Yhivb2un05qwMVbar_Qk|6|1031410010|1691992845104\"")
 	resp, err := client.Do(req)
 	if err != nil {
 		panic(fmt.Errorf("fail to send req: %w", err))
