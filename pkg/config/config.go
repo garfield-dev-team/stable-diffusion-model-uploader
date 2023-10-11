494ab935a196e9592707d46ec919aab3a3a80a5e
dongjiacheng <dongjiacheng@corp.netease.com>
2023-10-11 16:14:45 +0800

    feat: update

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,6 +3,6 @@ package config
 var (
 	Endpoint        = "https://oss-cn-hangzhou.aliyuncs.com"
 	BucketName      = "civitai-model-2"
-	AccessKeyId     = "LTAI5tBf3itErmHh969meim1"
-	AccessKeySecret = "Xrsl1d0Uekan2Ra0SFVpf0qJYPPpL5"
+	AccessKeyId     = "xxx"
+	AccessKeySecret = "xxx"
 )
